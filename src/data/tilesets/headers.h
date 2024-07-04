4d0f1090c12696c98f14777f8242a5518008de41
TheSylphIsIn <snacky42@gmail.com>
2024-07-03 23:23:57 -0400

    Install Poryscript + map work
    Added Poryscript, the "Shady Forest" tileset, and the first half of the route map.

--- src/data/tilesets/headers.h
+++ src/data/tilesets/headers.h
@@ -825,3 +825,14 @@ const struct Tileset gTileset_UnionRoom =
     .metatileAttributes = gMetatileAttributes_UnionRoom,
     .callback = NULL,
 };
+
+const struct Tileset gTileset_ShadyForest =
+{
+    .isCompressed = TRUE,
+    .isSecondary = TRUE,
+    .tiles = gTilesetTiles_ShadyForest,
+    .palettes = gTilesetPalettes_ShadyForest,
+    .metatiles = gMetatiles_ShadyForest,
+    .metatileAttributes = gMetatileAttributes_ShadyForest,
+    .callback = NULL,
+};
